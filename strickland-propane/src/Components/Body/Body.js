a4d26a2dd6dae5273e1fef5fccec5da33a2abb95
Haley <hjsulcer@gmail.com>
2018-10-27 19:05:37 -0400

    added temp banner image, arranged nav, and added quote

--- strickland-propane/src/Components/Body/Body.js
+++ strickland-propane/src/Components/Body/Body.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import BodyProduct from './BodyProduct.js';
 import './Body.css';
 
 class Body extends Component {
   render() {
     return (
       <div className="body">
-        <h1>Body</h1>
+
+        <div className="banner-img">
+          <img src="http://stricklandoil.com/wp-content/uploads/2013/01/3.jpg" alt="banner"></img>
+        </div>
+
+        <div className="quote">
+          <h1>"We sell propane and propane accessories."</h1>
+          <h1>- Hank Hill, Asst. Regional Manager</h1>
+        </div>
+
+        <BodyProduct />
+
+
+
       </div>
     );
   }
